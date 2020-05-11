cdaf38ca8cd5f1163a2533e339111e262f25d70b
Nandana <nandanasreeraj@gmail.com>
2020-05-11 12:24:31 +0530

    profilepage-rectified

--- pages/profilepage.jsx
+++ pages/profilepage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import BoxProfile from '../src/components/profile/profilebox';
+import ProjectProfile from '../src/components/profile/projectProfile';
 
 function Profile() {
   return (
     <div>
       
       <BoxProfile />
+      <ProjectProfile/>
     </div>
   );
 }
