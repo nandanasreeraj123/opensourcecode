250927fbd5c6f757717a4c6269d8f3f62ec012f7
Mohammed Rabeeh <mohammadrabeeh@gmail.com>
2020-05-12 18:23:00 +0530

    Fix Lint Errors

--- src/components/Feed/TopLang.jsx
+++ src/components/Feed/TopLang.jsx
@@ -1,10 +1,14 @@
-import React, { Component } from "react";
-import styles from "../../css/feed.module.css";
+import React, { Component } from 'react';
+import styles from '../../css/feed.module.css';
 
 class FeedLang extends Component {
-  state = {
-    showitems: false,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      showitems: false,
+    };
+  }
+
   dropDown = () => {
     this.setState((prevState) => ({
       showitems: !prevState.showitems,
@@ -12,48 +16,52 @@ class FeedLang extends Component {
   };
 
   render() {
+    const { showItems } = this.state;
     return (
-      <div className={styles["smallbox"]}>
-        <div className={styles["Language-Head"]}>
-          <div className={styles["disp-flex"]}>
-            <div className={styles["lang-space"]}>
+      <div className={styles.smallbox}>
+        <div className={styles['Language-Head']}>
+          <div className={styles['disp-flex']}>
+            <div className={styles['lang-space']}>
               <p>Top Languages</p>
             </div>
-            <div onClick={this.dropDown}>
-              <img src="SVG/dropdown-icon.svg" alt="dropdown"></img>
+            <div onClick={this.dropDown}> {/* eslint-disable-line */}
+              <img src="SVG/dropdown-icon.svg" alt="dropdown" />
             </div>
           </div>
         </div>
-        <div style={{ display: this.state.showitems ? "block" : "none" }}>
-          <div className={styles["Languages"]}>
-            <div className={styles["disp-flex"]}>
-              <div className={styles["Languages-num"]}>
+        <div style={{ display: showItems ? 'block' : 'none' }}>
+          <div className={styles.Languages}>
+            <div className={styles['disp-flex']}>
+              <div className={styles['Languages-num']}>
                 <p>1.</p>
               </div>
-              <div className={styles["Languages-js"]}>
+              <div className={styles['Languages-js']}>
                 <div>
-                <p >Javascript</p></div>
-                <div className={styles["small-follow"]}><p>Follow</p></div>
+                  <p>Javascript</p>
+                </div>
+                <div className={styles['small-follow']}><p>Follow</p></div>
               </div>
             </div>
-            <div className={styles["disp-flex"]}>
-              <div className={styles["Languages-num"]}>
+            <div className={styles['disp-flex']}>
+              <div className={styles['Languages-num']}>
                 <p>2.</p>
               </div>
-              <div className={styles["Languages-css"]}>
+              <div className={styles['Languages-css']}>
                 <div>
-                <p >CSS</p></div>
-                <div className={styles["small-follow"]}><p>Follow</p></div>
+                  <p>CSS</p>
+                </div>
+                <div className={styles['small-follow']}><p>Follow</p></div>
               </div>
             </div>
-            <div className={styles["disp-flex"]}>
-              <div className={styles["Languages-num"]}>
+            <div className={styles['disp-flex']}>
+              <div className={styles['Languages-num']}>
                 <p>3.</p>
               </div>
-              <div  className={styles["Languages-node"]}>
+              <div className={styles['Languages-node']}>
                 <div>
-                <p>Node</p></div>
-                <div className={styles["small-follow"]}><p>Follow</p></div>
+                  <p>Node</p>
+                </div>
+                <div className={styles['small-follow']}><p>Follow</p></div>
               </div>
             </div>
           </div>
--- src/components/Feed/TopOrg.jsx
+++ src/components/Feed/TopOrg.jsx
@@ -1,11 +1,15 @@
-import React, { Component } from "react";
-import styles from "../../css/feed.module.css";
+import React, { Component } from 'react';
+import styles from '../../css/feed.module.css';
 
 
 class FeedOrg extends Component {
-  state = {
-    showitems: false,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      showitems: false,
+    };
+  }
+
   dropDown = () => {
     this.setState((prevState) => ({
       showitems: !prevState.showitems,
@@ -13,48 +17,52 @@ class FeedOrg extends Component {
   };
 
   render() {
+    const { showItems } = this.state;
     return (
-      <div className={styles["smallbox"]}>
-        <div className={styles["Language-Head"]}>
-          <div className={styles["disp-flex"]}>
-            <div className={styles["lang-space"]}>
+      <div className={styles.smallbox}>
+        <div className={styles['Language-Head']}>
+          <div className={styles['disp-flex']}>
+            <div className={styles['lang-space']}>
               <p>Top Organisations</p>
             </div>
-            <div onClick={this.dropDown}>
-              <img src="SVG/dropdown-icon.svg" alt="dropdown"></img>
+            <div onClick={this.dropDown}> {/* eslint-disable-line */}
+              <img src="SVG/dropdown-icon.svg" alt="dropdown" />
             </div>
           </div>
         </div>
-        <div style={{ display: this.state.showitems ? "block" : "none" }}>
-          <div className={styles["Languages"]}>
-            <div className={styles["disp-flex"]}>
-              <div className={styles["Languages-num"]}>
+        <div style={{ display: showItems ? 'block' : 'none' }}>
+          <div className={styles.Languages}>
+            <div className={styles['disp-flex']}>
+              <div className={styles['Languages-num']}>
                 <p>1.</p>
               </div>
-              <div  className={styles["Languages-mozilla"]}>
+              <div className={styles['Languages-mozilla']}>
                 <div>
-                <p>Javascript</p></div>
-                <div className={styles["small-follow"]}><p>Follow</p></div>
+                  <p>Javascript</p>
+                </div>
+                <div className={styles['small-follow']}><p>Follow</p></div>
               </div>
             </div>
-            <div className={styles["disp-flex"]}>
-              <div className={styles["Languages-num"]}>
+            <div className={styles['disp-flex']}>
+              <div className={styles['Languages-num']}>
                 <p>2.</p>
               </div>
-              <div  className={styles["Languages-google"]}>
+              <div className={styles['Languages-google']}>
                 <div>
-                <p>CSS</p></div>
-                <div className={styles["small-follow"]}><p>Follow</p></div>
+                  <p>CSS</p>
+                </div>
+                <div className={styles['small-follow']}><p>Follow</p></div>
               </div>
             </div>
-            <div className={styles["disp-flex"]}>
-              <div className={styles["Languages-num"]}>
+            <div className={styles['disp-flex']}>
+              <div className={styles['Languages-num']}>
                 <p>3.</p>
               </div>
-              <div  className={styles["Languages-facebook"]}>
+              <div className={styles['Languages-facebook']}>
                 <div>
-                <p>Node</p></div>
-                <div className={styles["small-follow"]}><p>Follow</p></div>
+                  <p>Node</p>
+                </div>
+                <div className={styles['small-follow']}><p>Follow</p></div>
               </div>
             </div>
           </div>
