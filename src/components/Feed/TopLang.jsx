98ddf228f3b93a8b3043e08cb7a7fa744e6bd149
Nandana <nandanasreeraj@gmail.com>
2020-05-15 01:28:49 +0530

    new ui for profile page...updated

--- src/components/Feed/TopLang.jsx
+++ src/components/Feed/TopLang.jsx
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import styles from '../../css/feed.module.css';
 
 const FeedLang = () => {
   const [state, setState] = useState(false);
 
-=======
-import React, { useState } from "react";
-import styles from "../../css/feed.module.css";
-
-const FeedLang = () => {
-  const [state, setState] = useState(false);
-  
->>>>>>> upstream/master
   const dropDown = () => {
     if (state) {
       setState(false);
@@ -20,53 +11,6 @@ const FeedLang = () => {
       setState(true);
     }
   };
-<<<<<<< HEAD
-  return (
-    <div className={styles.smallbox}>
-      <div className={styles['Language-Head']}>
-        <div className={styles['disp-flex']}>
-          <div className={styles['lang-space']}>
-            <p>Top Languages</p>
-          </div>
-          <button type="button" onClick={dropDown} className={styles['btn-drop']}>
-            <img src="SVG/dropdown-icon.svg" alt="dropdown" />
-          </button>
-        </div>
-      </div>
-      <div style={{ display: state ? 'block' : 'none' }}>
-        <div className={styles.Languages}>
-          <div className={styles['disp-flex']}>
-            <div className={styles['Languages-num']}>
-              <p>1.</p>
-            </div>
-            <div className={styles['Languages-js']}>
-              <div>
-                <p>Javascript</p>
-              </div>
-              <div className={styles['small-follow']}><p>Follow</p></div>
-            </div>
-          </div>
-          <div className={styles['disp-flex']}>
-            <div className={styles['Languages-num']}>
-              <p>2.</p>
-            </div>
-            <div className={styles['Languages-css']}>
-              <div>
-                <p>CSS</p>
-              </div>
-              <div className={styles['small-follow']}><p>Follow</p></div>
-            </div>
-          </div>
-          <div className={styles['disp-flex']}>
-            <div className={styles['Languages-num']}>
-              <p>3.</p>
-            </div>
-            <div className={styles['Languages-node']}>
-              <div>
-                <p>Node</p>
-              </div>
-              <div className={styles['small-follow']}><p>Follow</p></div>
-=======
     return (
       <div className={styles["smallbox"]}>
         <div className={styles["Language-Head"]}>
@@ -110,18 +54,11 @@ const FeedLang = () => {
                 <p>Node</p></div>
                 <div className={styles["small-follow"]}><p>Follow</p></div>
               </div>
->>>>>>> upstream/master
             </div>
           </div>
         </div>
       </div>
-<<<<<<< HEAD
-    </div>
-  );
-};
-=======
     );
   }
->>>>>>> upstream/master
 
 export default FeedLang;
--- src/components/Feed/TopOrg.jsx
+++ src/components/Feed/TopOrg.jsx
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import styles from '../../css/feed.module.css';
-
-
-const FeedOrg = () => {
-  const [state, setState] = useState(false);
-
-=======
 import React, { useState } from "react";
 import styles from "../../css/feed.module.css";
 
@@ -14,7 +5,6 @@ import styles from "../../css/feed.module.css";
 const FeedOrg = ()=> {
   const [state, setState] = useState(false);
   
->>>>>>> upstream/master
   const dropDown = () => {
     if (state) {
       setState(false);
@@ -23,54 +13,6 @@ const FeedOrg = ()=> {
     }
   };
 
-<<<<<<< HEAD
-
-  return (
-    <div className={styles.smallbox}>
-      <div className={styles['Language-Head']}>
-        <div className={styles['disp-flex']}>
-          <div className={styles['lang-space']}>
-            <p>Top Organisations</p>
-          </div>
-          <button type="button" onClick={dropDown} className={styles['btn-drop']}>
-            <img src="SVG/dropdown-icon.svg" alt="dropdown" />
-          </button>
-        </div>
-      </div>
-      <div style={{ display: state ? 'block' : 'none' }}>
-        <div className={styles.Languages}>
-          <div className={styles['disp-flex']}>
-            <div className={styles['Languages-num']}>
-              <p>1.</p>
-            </div>
-            <div className={styles['Languages-mozilla']}>
-              <div>
-                <p>Mozilla</p>
-              </div>
-              <div className={styles['small-follow']}><p>Follow</p></div>
-            </div>
-          </div>
-          <div className={styles['disp-flex']}>
-            <div className={styles['Languages-num']}>
-              <p>2.</p>
-            </div>
-            <div className={styles['Languages-google']}>
-              <div>
-                <p>Google</p>
-              </div>
-              <div className={styles['small-follow']}><p>Follow</p></div>
-            </div>
-          </div>
-          <div className={styles['disp-flex']}>
-            <div className={styles['Languages-num']}>
-              <p>3.</p>
-            </div>
-            <div className={styles['Languages-facebook']}>
-              <div>
-                <p>Facebook</p>
-              </div>
-              <div className={styles['small-follow']}><p>Follow</p></div>
-=======
  
     return (
       <div className={styles["smallbox"]}>
@@ -115,18 +57,11 @@ const FeedOrg = ()=> {
                 <p>Facebook</p></div>
                 <div className={styles["small-follow"]}><p>Follow</p></div>
               </div>
->>>>>>> upstream/master
             </div>
           </div>
         </div>
       </div>
-<<<<<<< HEAD
-    </div>
-  );
-};
-=======
     );
   }
->>>>>>> upstream/master
 
 export default FeedOrg;
