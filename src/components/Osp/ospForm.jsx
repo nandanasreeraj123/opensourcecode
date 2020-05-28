5fb2ea3ff31a7b5b4a0c9023ea1ceaf2995c5165
Nandana <nandanasreeraj@gmail.com>
2020-05-29 02:57:46 +0530

    functionality

--- src/components/Osp/ospForm.jsx
+++ src/components/Osp/ospForm.jsx
@@ -1,59 +1,77 @@
-import React from "react";
+import React, { useState } from 'react';
 
-import styles from "../../css/osp.module.css";
+import styles from '../../css/osp.module.css';
 
-export default function FormOsp() {
+const FormOsp = () => {
+  const [tag, setTag] = useState('');
+  const [tags, setTags] = useState([]);
+
+  const onChange = (e) => {
+    setTag(e.target.value);
+  };
+
+  const removeTag = (indexToRemove) => {
+    setTags([...tags.filter((element, index) => index !== indexToRemove)]);
+  };
   return (
     <div>
       <div className={styles.bigBox}>
-        <div className={styles.flexing}>
+        <div className={styles.flexingin}>
           <div className={styles.qns}>
-            <div style={{ color: "blue" }}>
+            <div style={{ color: 'blue' }}>
               <p>Title</p>
             </div>
             <input
               className={styles.input}
               placeholder="Title of the project"
-              style={{ borderBottomColor: "blue" }}
+              style={{ borderBottomColor: 'blue' }}
             />
-            <div style={{ color: "#00CACA" }}>
+            <div style={{ color: '#00CACA' }}>
               <p>Description</p>
             </div>
             <input
               className={styles.input}
               placeholder="About your project"
-              
-              style={{ borderBottomColor: "#00CACA" }}
+              style={{ borderBottomColor: '#00CACA' }}
             />
-            <div style={{ color: "#0BA293" }}>
-            <div className={styles.flex}>
-                <div>
-                  <p>Skills</p>
-                </div>
-                <div>
-                <img src="SVG/plus.svg" alt="plus" />
-                </div>
-              </div>
+            <div style={{ color: '#0BA293' }}>
+              <p>Skills</p>
             </div>
-            
-            <input
-              className={styles.input}
-              id="myInput"
-              placeholder="Enter the skills"
-              style={{ borderBottomColor: "#0BA293" }}
-            />
-            
-            <div className={styles.flexingskills}>
-              <div className={styles.skill} style={{ borderColor: "red" }}>
-                <p>Node</p>
-              </div>
-              <div className={styles.skill} style={{ borderColor: "#00CACA" }}>
-                <p>React</p>
-              </div>
-            </div>
+            <form
+              className={styles.skills}
+              onSubmit={(e) => {
+                setTags([...tags, tag]);
+                setTag('');
+                e.target.reset();
+                e.preventDefault();
+              }}>
+              <input
+                required
+                className={styles.input}
+                id="myInput"
+                placeholder="Enter the skills"
+                style={{ borderBottomColor: '#0BA293' }}
+                onKeyUp={(e) => onChange(e)}
+              />
+              <input type="submit" className={styles.addButton} value="+" />
+            </form>
+            <ul className={styles.skillList}>
+              {tags.map((Tag, index) => (
+                <div key={Tag} className={styles.skill}>
+                  <li>{Tag}</li>
+                  <div
+                    role="button"
+                    tabIndex={0}
+                    onClick={() => removeTag(index)}
+                    onKeyDown={() => removeTag(index)}>
+                    x
+                  </div>
+                </div>
+              ))}
+            </ul>
           </div>
           <div className={styles.qns}>
-            <div style={{ color: "black" }}>
+            <div style={{ color: 'black' }}>
               <div className={styles.flex}>
                 <div>
                   <p>Github</p>
@@ -66,10 +84,10 @@ export default function FormOsp() {
             <input
               className={styles.input}
               placeholder="www.github.com/username"
-              style={{ borderBottomColor: "black" }}
+              style={{ borderBottomColor: 'black' }}
             />
-            <div style={{ color: "#9F0BDD" }}>
-            <div className={styles.flex}>
+            <div style={{ color: '#9F0BDD' }}>
+              <div className={styles.flex}>
                 <div>
                   <p>Hosting URL</p>
                 </div>
@@ -81,16 +99,32 @@ export default function FormOsp() {
             <input
               className={styles.input}
               placeholder="Hosting URL of the project"
-              style={{ borderBottomColor: "#9F0BDD" }}
+              style={{ borderBottomColor: '#9F0BDD' }}
             />
             <p>Upload Your Project Here</p>
-            <img src="SVG/Group 142.svg" alt="upload" className={styles.upload} />
+
+            <label htmlFor="file">
+              <img
+                src="SVG/Group 142.svg"
+                alt="upload"
+                className={styles.upload}
+              />
+              <input
+                type="file"
+                name="file"
+                id="file"
+                style={{ display: 'none' }}
+              />
+            </label>
           </div>
         </div>
         <div>
-            <button type="button" className={styles["enter-button"]}>Submit</button>
+          <button type="button" className={styles['enter-button']}>
+            Submit
+          </button>
         </div>
       </div>
     </div>
   );
-}
+};
+export default FormOsp;
